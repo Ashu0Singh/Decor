c0dff30a359555043365c49a12f0dfcab1946b0e
Ashu0Singh <ashusingh1417@gmail.com>
2023-11-23 00:32:48 +0530

    Updating

--- lib/models/User.ts
+++ lib/models/User.ts
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
 	{
-		fullname: { type: String, required: true, unique: true },
-		email: { type: String, required: true },
+		fullname: { type: String, required: true },
+		email: { type: String, required: true, unique: true },
 		password: { type: String, required: true },
 		refreshtoken: { type: String },
 		generatedSuggestions: [
